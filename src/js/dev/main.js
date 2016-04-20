163da1714f562cbe3ed28a83e08c804add04b2a5
Sebastian Bailey <sebastianbailey@Sebastians-iMac.local>
2016-04-20 17:21:05 +0100

    Insta / Lastfm
    Adding LastFM feed, and refining Instagram and LastFM feed on contact
    page.

--- src/js/dev/main.js
+++ src/js/dev/main.js
@@ -27,18 +27,24 @@ app.config(function($routeProvider, $locationProvider){
 });
 
 app.factory('instagram', ['$http', function($http){
-
 	return {
-		fetchPopular: function(callback){
-            
+		fetchData: function(callback){
             var endPoint = "https://api.instagram.com/v1/users/230978815/media/recent/?access_token=230978815.467ede5.1e39f843afb441068cdeb30c8747c12b&callback=JSON_CALLBACK";
-            
             $http.jsonp(endPoint).success(function(response){
                 callback(response.data);
             });
 		}
 	}
-
+}]);
+app.factory('lastfm', ['$http', function($http) {
+	var topArtists = {};
+	topArtists.getArtists = function() {
+		return $http({
+			method: 'GET',
+			url: 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&limit=15&user=sebcglbailey&api_key=facf48415f1494c1a72a48ca0055093b&format=json'
+		});
+	}
+	return topArtists;
 }]);
 
 app.controller('mainController', function($scope, $routeParams) {
@@ -74,15 +80,28 @@ app.controller('aboutController', function($scope, $routeParams) {
     	$('#header').removeClass('clicked');
     });
 });
-app.controller('contactController', ['$scope', 'instagram' , function ($scope, instagram) {
+app.controller('contactController', ['$scope', 'instagram', 'lastfm', function ($scope, instagram, lastfm) {
 	$scope.name = "contactController";
 	$scope.contactList = contact;
 	$scope.pics = [];
-	instagram.fetchPopular(function(data){
+	instagram.fetchData(function(data){
 		$scope.pics = data;
+		$scope.activeItemIn = data[0];
 	});
-	$scope.setActive = function(item) {
-    	$scope.activeItem = item;
+	$scope.topArtists = [];
+	lastfm.getArtists().success(function(response) {
+		$scope.topArtists = response;
+		$scope.activeItemLf = response.topartists.artist[0];
+	});
+	$scope.setActiveIn = function(item) {
+    	$scope.activeItemIn = item;
+    	var instaCards = $('#insta-cards').offset().top - 95;
+    	$('html, body').animate({scrollTop: instaCards});
+    };
+    $scope.setActiveLf = function(item) {
+    	$scope.activeItemLf = item;
+    	var lastFmCards = $('#lastfm-cards').offset().top - 95;
+    	$('html, body').animate({scrollTop: lastFmCards});
     };
 	$scope.$on('$viewContentLoaded', function(){
     	$('#header').removeClass('clicked');
