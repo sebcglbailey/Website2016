0b353c0ac64657cd4579303f5c6a282a7cb920d3
Sebastian Bailey <sebastianbailey@Sebastians-iMac.local>
2016-06-15 10:47:34 +0100

    mistake with web link fix

--- src/js/dev/projects.js
+++ src/js/dev/projects.js
@@ -500,7 +500,7 @@ var projects = [
 		type: ['ux', 'User Experience'],
 		title: 'Palringo WebUI Client',
 		description: [
-			'Designing the web-client for the Palringo social gaming app. As part of the creative team, myself and one other designer helped shape the overall look and feel, and User Experience of the <a href="http:/chat.palringo.com" title="Palringo WebUI Client" target="_blank">new web-client for the app.</a>',
+			'Designing the web-client for the Palringo social gaming app. As part of the creative team, myself and one other designer helped shape the overall look and feel, and User Experience of the <a href="http://chat.palringo.com" title="Palringo WebUI Client" target="_blank">new web-client for the app.</a>',
 			'Not being a native app, we were not constricted to best practice standards, and could experiment with changing the UX from the current application. The chat became much more fluid, allowing new users to get straight into the core functionality of the platform, and also allowing users to flick between different groups with ease.'
 		],
 		coverImage: {
